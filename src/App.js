fed7f6e8026ad61b6662a0c2e9a0efabf85af780
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2021-06-13 18:51:43 +0300

    k

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
-import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions'
-import Statistics from './components/Statistics/Statistics'
-import Section from './components/Section/Section'
+import FeedbackOptions from './components/FeedbackOptions'
+import Statistics from './components/Statistics'
+import Section from './components/Section'
 
 import S from './components/Section/Section.module.css'
 
